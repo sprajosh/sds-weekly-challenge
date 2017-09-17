1dfaac28cd5e941476c87c8a0002e52eefd4aeb1
Siddharth <sprajosh@gmail.com>
2017-09-17 18:31:38 +0530

    Week 3 Challenge - ceaser cipher

---
+++ ceaser.py
@@ -0,0 +1,15 @@
+alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+s = input("Enter string: ")
+key = int(input("Enter key: "))
+while key > 26:
+	key-=26
+for x in set(s):
+	if x in alpha:
+		new_char = ord(x)+key
+		if (97 <= new_char <= 122) or (65 <= new_char <= 90):
+			s = s.replace(x, chr(new_char))
+		elif new_char > 122:
+			s = s.replace(x, chr(new_char-26))
+		elif new_char > 90:
+			s = s.replace(x, chr(new_char-26))
+print(s)
