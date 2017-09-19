9ffc88120cbc03bd6a04ae0fdb4e7034d1ee095a
Siddharth <sprajosh@gmail.com>
2017-09-19 21:19:43 +0530

    Correct logical errors

--- ceaser.py
+++ ceaser.py
@@ -1,13 +1,16 @@
 alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 s = input("Enter string: ")
 key = int(input("Enter key: "))
+answer = []
 while key > 26:
 	key-=26
-for x in set(s):
+for x in (s):
 	if x in alpha:
 		new_char = ord(x)+key
-		if (97 <= new_char <= 122) or (65 <= new_char <= 90):
-			s = s.replace(x, chr(new_char))
+		if (97 <= ord(x) <= 122 and new_char <= 122) or (65 <= ord(x) <= 90 and new_char <= 90):
+			answer.append(chr(new_char))
 		else:
-			s = s.replace(x, chr(new_char-26))
-print(s)
+			answer.append(chr(new_char-26))
+	else:
+		answer.append(x)
+print(('').join(answer))
