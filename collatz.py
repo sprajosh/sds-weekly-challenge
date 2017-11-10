79f401665127bcdef7d2c915650061f1697ce3c2
sprajosh <sprajosh@gmail.com>
2017-11-10 23:01:00 +0530

    Fix intendation errors

--- collatz.py
+++ collatz.py
@@ -5,18 +5,18 @@ max_chain = 0
 def collat(n):
 	count = 0
 	while n != 1 and n not in collatz:
-    	if n%2 == 0:
-        	n = n/2
-		else:
-        	n = 3*n + 1
-            count += 1
-        if n in collatz:
-            count += collatz[n]
-        return count
+	if n%2 == 0:
+		n = n/2
+	else:
+		n = 3*n + 1
+		count += 1
+	if n in collatz:
+		count += collatz[n]
+	return count
 
 for x in xrange(1, 1000001):
-        collatz[x] = collat(x)
-        if collatz[x] > max_chain:
-			max_value, max_chain = x, collatz[x]
-			print (max_value, collatz[x])
+	collatz[x] = collat(x)
+	if collatz[x] > max_chain:
+		max_value, max_chain = x, collatz[x]
+		print (max_value, collatz[x])
 print (max_value, max_chain)
